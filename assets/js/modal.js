2ced0895d0079658a5052c50c0c74c57ae4ab4a5
Eve Schuetze <esc-berlin@outlook.de>
2024-09-01 18:17:13 +0200

    feat: fill the modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,4 +1,4 @@
-var modal = document.getElementById("dwarf-digger-modal");
+var modal = document.getElementById("modal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("dwarf-digger-button");
@@ -6,14 +6,47 @@ var btn = document.getElementById("dwarf-digger-button");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-var modalImage = document.getElementsByClassName("modal-image");
-
 var testImageSource = "images/NightLifeDriveGame.gif";
+
+var modalImage = document.getElementsByClassName("modal-image");
+var descriptionHeader = document.getElementById("modal-project-description-header");
+var descriptionContent = document.getElementById("modal-project-descriptions-content");
+var responsibilitiesContent = document.getElementById("modal-project-responsibilities-content");
+var linkContent = document.getElementById("modal-project-links-content");
+
+const NightLifeArray = ["Night Life Drive",
+    "A 3D endless racer with an emphasis on driving to synthwave music. This was a group project coded in Unity and\n" +
+    "\t\t\t\t\t\t\t\t\tI was responsible for the music player, the UI design and implementation, creating the main menu screen and the textures of the floor, including lighting map.",
+    "UI Design",
+    "Design Cohesion",
+    "Music & Sound Mixing",
+    "Github",
+    ""
+]
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "flex";
+  modal.style.display = "block";
+  addContent(NightLifeArray);
   modalImage.src = testImageSource.src;
+}
+
+function addContent(contentArray) {
+  descriptionHeader.textContent = contentArray[0];
+  descriptionContent.textContent = contentArray[1];
+  for (let i = 2; i <= 4; i++) {
+    var descriptionNode = document.createElement("li");
+    var textNode = document.createTextNode(contentArray[i]);
+    descriptionNode.appendChild(textNode);
+    responsibilitiesContent.appendChild(descriptionNode);
+  }
+  var linkNode = document.createElement("a");
+  var linkTextNode = document.createTextNode(contentArray[5]);
+  linkNode.appendChild(linkTextNode);
+  linkNode.href = contentArray[6];
+  linkNode.target = "_blank"
+  linkContent.appendChild(linkNode);
+
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -23,7 +56,7 @@ span.onclick = function() {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target === modal) {
     modal.style.display = "none";
   }
 }
